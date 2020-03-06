535dc1d18a8cece5c44219c442d3ce8c5a944e50
Rob Lawrence <iamrobnyce@gmail.com>
2020-03-06 22:47:58 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,28 +1,14 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
-
-require 'pp'
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# using director_data as input
-def gross_for_director(director_data)
-
-  total = 0
-  director_data[:movies].each do |movie|
-    total += movie[:worldwide_gross]
-  end
-  total
 end
 
-# Write a method that, given an NDS creates a new Hash
-@@ -13,5 +17,10 @@ def gross_for_director(director_data)
-# { directorOne => allTheMoneyTheyMade, ... }
-def directors_totals(nds)
-  result = {}
-  nil
-  nds.each do |director|
-    pp result[director[:name]] = gross_for_director(director)
-  end
-
-  pp result
-
+def print_first_directors_movie_titles
+  i=0 
+  count=0 
+  movies = directors_database[0][:movies]
+  while i < movies.count do 
+    puts movies [i][:title]
+    i += 1
+  while count < movies.count do 
+    puts movies [count][:title]
+    count += 1
+end 
 end
